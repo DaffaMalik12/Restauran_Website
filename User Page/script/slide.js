08429b9788fbd9e21062649e5fb6fa610dd89a4e
DaffaMalik12 <dmalikakram@gmail.com>
2024-01-11 13:32:59 +0700

    lupa perubahan apa aja

---
+++ User Page/script/slide.js
@@ -0,0 +1,15 @@
+var paginationPertama = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// const listOfCardElements = document.querySelectorAll(".card");
+// const cardContainer = document.querySelector(".card-container");
+
+// listOfCardElements.forEach((cardElement, index) => {
+//   cardElement.addEventListener("click", () => {
+//     const scrollLeft = index * listOfCardElements[0].offsetWidth;
+//     cardContainer.scrollTo({ left: scrollLeft, behavior: "smooth" });
+//   });
+// });
