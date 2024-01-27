8855af7d85ac9ffbb9591dc423b55f1a9da2395e
DaffaMalik12 <dmalikakram@gmail.com>
2024-01-27 22:08:48 +0700

    penambahan logic

---
+++ User Page/script/logic_lavacake.js
@@ -0,0 +1,36 @@
+let jumlahPesanan = 1;
+let hargaPerItem = 22000; // Harga per item dalam Rupiah
+
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const num = document.querySelector(".num");
+const harga = document.querySelector(".harga");
+const currency = document.querySelector(".currency");
+
+plus.addEventListener("click", () => {
+  jumlahPesanan++;
+  updateNumAndHarga();
+});
+
+minus.addEventListener("click", () => {
+  if (jumlahPesanan > 1) {
+    jumlahPesanan--;
+    updateNumAndHarga();
+  }
+});
+
+function updateNumAndHarga() {
+  // Format jumlahPesanan menjadi dua digit angka
+  let formattedJumlah =
+    jumlahPesanan < 10 ? "0" + jumlahPesanan : jumlahPesanan;
+
+  // Hitung total harga
+  let totalHarga = jumlahPesanan * hargaPerItem;
+
+  // Format harga menjadi mata uang Rupiah
+  let formattedHarga = totalHarga.toLocaleString("id-ID");
+
+  // Perbarui tampilan jumlah pesanan dan harga
+  num.innerText = formattedJumlah;
+  harga.innerText = formattedHarga;
+}
